fbd064b30df0126619f373525a468facb72a5352
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-02 01:51:30 +0200

    TP Paramètres JVM

--- src/main/java/fr/diginamic/jvm/TestMemoire.java
+++ src/main/java/fr/diginamic/jvm/TestMemoire.java
@@ -1,10 +1,30 @@
 package fr.diginamic.jvm;
 
+import java.util.ArrayList;
+
 public class TestMemoire {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ArrayList<String> listeString = new ArrayList<String>();
+		boolean arret = false;
+		int i = 1;
+		
+		while ( !arret ) {
+			listeString.add("k");
+			
+			if (i % 10000 == 0) {
+				System.out.println(listeString.size());
+			}
+			
+			i++;
+		}
 
+		
+		/*Quand on compile avec Xmx100k on obtient l'erreur suivante : 
+		 * Error occurred during initialization of VM
+		 * Too small maximum heap
+		 */
 	}
 
 }
